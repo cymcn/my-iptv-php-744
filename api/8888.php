13b72f538e9bb4e37da09dfeb8f9e3994fa53b91
cym <95155750+cymcn@users.noreply.github.com>
2023-11-28 16:02:57 +0800

    Update 8888.php

--- api/8888.php
+++ api/8888.php
@@ -6,11 +6,14 @@ $n = [
     'cctv1' => '3221225804', // CCTV1
     'cctv2' => '3221226195', // CCTV2
     'cctv3' => '3221226397', // CCTV3
+    '风云足球' => '3221226984', // 风云足球
+    'ystq' => '3221226956', // 风云足球
 ];
 $ipArray = [
     'dbiptv.sn.chinamobile.com',
 ];
 $ip = $ipArray[array_rand($ipArray)];
-$playurl = "http://{$ip}/PLTV/88888890/224/{$n[$id]}/index.m3u8";
+$playurl = "http://{$ip}/PLTV/88888893/224/{$n[$id]}/index.m3u8";
+$playurl2 = "http://{$ip}/PLTV/88888890/224/{$n[$id]}/index.m3u8"; // 新增的播放地址
 header('Location: ' . $playurl);
 exit;
