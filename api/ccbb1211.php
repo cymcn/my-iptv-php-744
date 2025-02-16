2f862967386d36032f53c1f4b0c301d2a87920b6
cym <95155750+cymcn@users.noreply.github.com>
2025-02-16 12:46:50 +0800

    更新 ccbb1211.php

---
+++ api/ccbb1211.php
@@ -0,0 +1,54 @@
+<?php
+header("Content-Type: text/json; charset=UTF-8");
+$id = isset($_GET['id'])?$_GET['id']:'fhzx';
+$tv = array(
+  'fhzx' => '4',  //資 訊 台
+  'fhzw' => '5',  //中 文 台
+  'fhhk' => '6',  //香 港 台
+  );
+$url = 'http://m.fengshows.com/api/v3/live?live_type=tv';
+$response = get_data($url);
+$channels = json_decode($response);
+foreach ($channels as $channel) {
+  if($channel->order==$tv[$id]){
+    $channelId = $channel->_id;
+    break; 
+  }    
+}
+$info = get_url($channelId,'FHD');
+if($info->status !== '0'){
+  $info = get_url($channelId,'HD');
+}
+$liveUrl = $info->data->live_url;
+header('Location:'.$liveUrl);
+
+function get_url($cid, $qa){
+  $url = "https://m.fengshows.com/api/v3/hub/live/auth-url?live_id={$cid}&live_qa={$qa}";
+  $response = get_data($url);
+  $data = json_decode($response);
+  return $data;
+}
+
+function get_data($url){
+$header = [
+  'fengshows-client: app(ios,5040718)',
+  'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 15_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/98.0.4758.85 Mobile/15E148 Safari/604.1',
+  'token:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiJlZGM5MTlhMC1lYmE3LTExZWYtODQ1My0xYmVhN2Y1MDI4ZDciLCJuYW1lIjoi57K-576O55qE6I2U5p6dMDU4IiwidmlwIjowLCJqdGkiOiJMb1ZVeGNPNVgiLCJyZWdpc3RyYXRpb25faWQiOiIxNjFhMzc5N2M5Mzg4ZTZjOGY3IiwiaWF0IjoxNzM5NjM4NDg1LCJleHAiOjE3NDIyMzA0ODV9.if4kvP6nqx5ATnBY3GahdwJbYoMnhq0M5NuNaMaRCZ0'
+];
+
+$options = [
+    'http' => [
+        'method' => 'GET',
+        'header' => implode("\r\n", $header)
+    ]
+];
+
+$context = stream_context_create($options);
+$response = file_get_contents($url, false, $context);
+
+if ($response === FALSE) {
+    die('Error fetching data');
+}
+
+return $response;
+}
